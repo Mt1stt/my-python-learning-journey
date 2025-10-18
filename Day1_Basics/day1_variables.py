3f8a0c676f9874c5eceb579f3b57f054d31607dd
Mt1stt <mohamadtamer91@gmail.com>
2025-10-18 11:57:01 +0300

    Finished Day1 variables exercise

--- Day1_Basics/day1_variables.py
+++ Day1_Basics/day1_variables.py
@@ -6,3 +6,17 @@ height=1.79
 weight=90
 BMI=weight/height**2
 print(BMI)
+print(type(y))
+monthly_savings=10 
+num_months=4
+new_savings=monthly_savings * num_months
+print(new_savings)
+savings = 100
+new_savings = 40
+
+# Calculate total_savings using savings and new_savings
+total_savings = savings + new_savings
+print(total_savings)
+
+# Print the type of total_savings
+print(type(total_savings))
